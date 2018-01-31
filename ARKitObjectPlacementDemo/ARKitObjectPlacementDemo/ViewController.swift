f1da80ca4505bae8f2be5ca955797442df2cba09
nauman-tintash <nauman@tintash.com>
2018-01-31 12:08:11 +0500

    Added the session delegate and a status label in the scene to show the user the tracking state all the time.

--- ARKitObjectPlacementDemo/ARKitObjectPlacementDemo/ViewController.swift
+++ ARKitObjectPlacementDemo/ARKitObjectPlacementDemo/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var sessionInfoView: UIView!
     @IBOutlet weak var sessionInfoLabel: UILabel!
     
@@ -20,8 +21,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
+        // Set a delegate to track the number of plane anchors for providing UI feedback.
+        sceneView.session.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
@@ -97,37 +98,70 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - ARSessionDelegate
     
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-//        guard let frame = session.currentFrame else { return }
-//        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
     }
     
     func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
-//        guard let frame = session.currentFrame else { return }
-//        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
     }
     
     func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
-//        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
     }
     
     // MARK: - ARSessionObserver
     
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+        // Present an error message to the user.
+        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        startNewSession()
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+        sessionInfoLabel.text = "Session was interrupted"
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        sessionInfoLabel.text = "Session interruption ended"
+        startNewSession()
     }
     
     // MARK: - Private Functions.
+    
+    private func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        // Update the UI to provide feedback on the state of the AR experience.
+        let message: String
+        
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            // No planes detected; provide instructions for this app's AR interactions.
+            message = "Move the device around to detect horizontal surfaces."
+            
+        case .normal:
+            // No feedback needed when tracking is normal and planes are visible.
+            message = ""
+            
+        case .notAvailable:
+            message = "Tracking unavailable."
+            
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+            
+        case .limited(.initializing):
+            message = "Initializing AR session."
+            
+        }
+        
+        sessionInfoLabel.text = message
+        sessionInfoView.isHidden = message.isEmpty
+    }
     
     //Start a new session after resettng tracking and removing existing anchors.
     private func startNewSession() {
